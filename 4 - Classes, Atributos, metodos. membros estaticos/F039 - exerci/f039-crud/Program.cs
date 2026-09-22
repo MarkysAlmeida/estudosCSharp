222be0d82515d9b0943cc42cb9fa9b9b54391cc1
MarkysAlmeida <marcos2011.almeidas@gmail.com>
2026-09-21 21:26:27 -0300

    Objetivo da aula era fazer um programa que recebe-se um produto com nome, valor e quantidade, pois ir apresentando as mudança quando adiciona-se mais estoque, o que muda é valor do estoque e do preço total. - tentativa solo, sem professor

---
+++ 4 - Classes, Atributos, metodos. membros estaticos/F039 - exerci/f039-crud/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso;
+public class Produto {
+    public string nome;
+    public double preco;
+    public int quantidade;
+}
+class CRUDProgram {
+    static void Main(string[] args) {
+
+        Produto nome = new Produto();
+        Produto preco = new Produto();
+        Produto quantidade = new Produto();
+        Produto addProduto = new Produto();
+        Produto removeProduto = new Produto();
+
+        Console.WriteLine("Entre os dados do produto");
+        Console.Write("Nome: ");
+        nome.nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        preco.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        quantidade.quantidade = int.Parse(Console.ReadLine());
+
+        double somaproduto = preco.preco * quantidade.quantidade;
+        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        //Segunda parte - Adicionar estoque
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        addProduto.quantidade = int.Parse(Console.ReadLine());
+        quantidade.quantidade = quantidade.quantidade + addProduto.quantidade;
+        somaproduto = preco.preco * quantidade.quantidade;
+
+        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
+
+        //Terceira parte - remover Estoque
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        removeProduto.quantidade = int.Parse(Console.ReadLine());
+        quantidade.quantidade = quantidade.quantidade - removeProduto.quantidade;
+        somaproduto = preco.preco * quantidade.quantidade;
+
+        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
+
+
+    }//end static
+}
+
