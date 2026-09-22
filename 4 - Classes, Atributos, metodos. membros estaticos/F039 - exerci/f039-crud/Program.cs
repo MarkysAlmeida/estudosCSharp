2a6c2395727c6c2dadc2f052131de1e0511f45d2
MarkysAlmeida <marcos2011.almeidas@gmail.com>
2026-09-22 06:40:09 -0300

    Projeto revisado com os novos aprendizados

--- 4 - Classes, Atributos, metodos. membros estaticos/F039 - exerci/f039-crud/Program.cs
+++ 4 - Classes, Atributos, metodos. membros estaticos/F039 - exerci/f039-crud/Program.cs
@@ -1,50 +1,46 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Curso;
-public class Produto {
-    public string nome;
-    public double preco;
-    public int quantidade;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser ADICIONADO ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser REMOVIDO do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProduto(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+
+    }
 }
-class CRUDProgram {
-    static void Main(string[] args) {
-
-        Produto nome = new Produto();
-        Produto preco = new Produto();
-        Produto quantidade = new Produto();
-        Produto addProduto = new Produto();
-        Produto removeProduto = new Produto();
-
-        Console.WriteLine("Entre os dados do produto");
-        Console.Write("Nome: ");
-        nome.nome = Console.ReadLine();
-        Console.Write("Preço: ");
-        preco.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.Write("Quantidade no estoque: ");
-        quantidade.quantidade = int.Parse(Console.ReadLine());
-
-        double somaproduto = preco.preco * quantidade.quantidade;
-        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
-
-
-        //Segunda parte - Adicionar estoque
-        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-        addProduto.quantidade = int.Parse(Console.ReadLine());
-        quantidade.quantidade = quantidade.quantidade + addProduto.quantidade;
-        somaproduto = preco.preco * quantidade.quantidade;
-
-        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
-
-        //Terceira parte - remover Estoque
-        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-        removeProduto.quantidade = int.Parse(Console.ReadLine());
-        quantidade.quantidade = quantidade.quantidade - removeProduto.quantidade;
-        somaproduto = preco.preco * quantidade.quantidade;
-
-        Console.WriteLine("Dados do Produto: " + nome.nome + ", $" + preco.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + quantidade.quantidade + " unidades, Total: " + somaproduto.ToString("F2", CultureInfo.InvariantCulture));
-
-
-    }//end static
-}
-
